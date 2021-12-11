73d328ca6c911a28df81731d90f720c12f5f6bdd
Doni <donihernandez003@gmail.com>
2021-12-10 22:55:00 -0500

    Mounting Reviews and Location

---
+++ components/Details/TabDetails/Location/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import styles from './styles';
+
+const Location = () => {
+  return (
+    <View>
+      <Text>Location</Text>
+    </View>
+  );
+};
+
+export default Location;
--- components/Details/TabDetails/Tabs/index.js
+++ components/Details/TabDetails/Tabs/index.js
@@ -3,9 +3,8 @@ import {View, useWindowDimensions} from 'react-native';
 import {TabView, SceneMap, TabBar} from 'react-native-tab-view';
 import COLORS from '../../../../constants/colors';
 import MenuList from '../MenuList';
-
-const SecondRoute = () => <View style={{flex: 1, backgroundColor: '#fff'}} />;
-const ThirdRoute = () => <View style={{flex: 1, backgroundColor: '#fff'}} />;
+import Reviews from '../Reviews';
+import Location from '../Location';
 
 const renderTabBar = props => (
   <TabBar
@@ -16,21 +15,20 @@ const renderTabBar = props => (
 );
 
 export default function TabDetails({foods}) {
-  const FirstRoute = () => <MenuList foods={foods} />;
+  const FirstRoute = () => <Reviews />;
+  const SecondRoute = () => <Location />;
 
   const renderScene = SceneMap({
     first: FirstRoute,
     second: SecondRoute,
-    third: ThirdRoute,
   });
 
   const layout = useWindowDimensions();
 
   const [index, setIndex] = useState(0);
   const [routes] = useState([
-    {key: 'first', title: 'Menu'},
-    {key: 'second', title: 'Reviews'},
-    {key: 'third', title: 'Location'},
+    {key: 'first', title: 'Reviews'},
+    {key: 'second', title: 'Location'},
   ]);
 
   return (
